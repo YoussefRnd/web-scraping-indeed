18a5bc05104aa6e4685cdbfdd573ae9afb9234bb
YoussefRnd <boumlaki2002@gmail.com>
2023-01-24 21:29:33 +0100

    add data to csv

--- indeed.py
+++ indeed.py
@@ -1,41 +1,69 @@
+"""This script scrapes job offers from indeed.com"""
 import csv
 
 import cloudscraper
-import requests
 from bs4 import BeautifulSoup
 
+# headers for the request
 headers = {
-    "x-api-key": "cf82404402779d28ecfd584bb1295496",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
     "Accept": "text/html",
     "Referer": "https://www.example.com",
     "Connection": "keep-alive",
     "Accept-Encoding": "gzip, deflate",
 }
-# jog = input("Enter the job you want to search for: ")
-# country = input("Enter the country you want to search in: ")
+offer = input("Enter the job you want to search for: ")
+country = input("Enter the country you want to search in: ")
 scraper = cloudscraper.create_scraper(delay=10, browser="chrome")
 page = scraper.get(
-    f"https://ma.indeed.com/jobs?q=python&l=Tanger&vjk=67d06dd02bdaf629",
+    f"https://ma.indeed.com/jobs?q={offer.replace(' ','+')}&l={country.replace(' ','+')}&start=60",
     headers=headers,
 )
+print("======please wait======")
+job_data = []
 
 
 def get_job_info():
-    src = page.content
-    soup = BeautifulSoup(src, "html.parser")
-    job_data = []
+    """This function gets the job information"""
+    soup = BeautifulSoup(page.content, "html.parser")
+
     job_offer = soup.find_all("div", {"class": "cardOutline"})
     for job in job_offer:
         job_title = job.find("h2", {"class": "jobTitle"}).text.strip()
+
         job_company = job.find("div", {"class": "company_location"}).find(
             "span", {"class": "companyName"}
         )
         if job_company is None:
-            job_company = "Company is not specified"
+            job_company = "Not specified"
         else:
             job_company = job_company.text.strip()
-        print(job_company)
+        job_description = job.find("div", {"class": "job-snippet"}).text.strip()
+
+        job_type = job.find("div", {"class": "attribute_snippet"})
+        if job_type is None:
+            job_type = "Not specified"
+        else:
+            job_type = job_type.text.strip()
+
+        job_last_update = job.find("span", {"class": "date"}).text.strip()
+
+        job_data.append(
+            {
+                "Job title": job_title,
+                "Company": job_company,
+                "Description": job_description,
+                "Job type": job_type,
+                "Last update": job_last_update,
+            }
+        )
 
 
 get_job_info()
+# save the data in a csv file
+keys = job_data[0].keys()
+with open("jobs.csv", "w", newline="", encoding="utf-8") as csv_file:
+    writer = csv.DictWriter(csv_file, keys)
+    writer.writeheader()
+    writer.writerows(job_data)
+    print("======file created successfully======")
