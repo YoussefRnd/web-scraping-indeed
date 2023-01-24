8d7286dd1c4afbb589ff94d6b2f85e3d6c87ea0d
YoussefRnd <boumlaki2002@gmail.com>
2023-01-24 23:44:06 +0100

    add some stuff

--- indeed.py
+++ indeed.py
@@ -4,6 +4,7 @@ import csv
 import cloudscraper
 from bs4 import BeautifulSoup
 
+
 # headers for the request
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
@@ -12,20 +13,22 @@ headers = {
     "Connection": "keep-alive",
     "Accept-Encoding": "gzip, deflate",
 }
-offer = input("Enter the job you want to search for: ")
-country = input("Enter the country you want to search in: ")
+offer = input("Enter the job you want to search for: ").strip()
+country = input("Enter the country you want to search in: ").strip()
 scraper = cloudscraper.create_scraper(delay=10, browser="chrome")
 page = scraper.get(
-    f"https://ma.indeed.com/jobs?q={offer.replace(' ','+')}&l={country.replace(' ','+')}&start=60",
+    f"https://indeed.com/jobs?q={offer.replace(' ','+')}&l={country.replace(' ','+')}",
     headers=headers,
 )
+
+
 print("======please wait======")
 job_data = []
 
 
 def get_job_info():
     """This function gets the job information"""
-    soup = BeautifulSoup(page.content, "html.parser")
+    soup = BeautifulSoup(page.content, "lxml")
 
     job_offer = soup.find_all("div", {"class": "cardOutline"})
     for job in job_offer:
